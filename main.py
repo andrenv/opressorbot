9192eab4c4ec4e4f0277219280f50322911b61f9
andrenv <andrenv@users.noreply.github.com>
2021-09-18 02:01:46 +0000

    remove try

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ ytdl_format_options = {
     'noplaylist': True,
     'nocheckcertificate': True,
     'ignoreerrors': False,
-    'logtostderr': False,
+    'logtostderr': True,
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
@@ -74,45 +74,18 @@ async def microaggression(ctx):
     voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
   await ctx.send('Did just hear someone use a microaggression')
 
-
-@client.command(pass_context=True)
-async def pqp(ctx):
-  
-  url = 'https://www.myinstants.com/instant/bolsonaro-palavroes-32791/?utm_source=copy&utm_medium=share'
-  server = ctx.message.guild
-  voice_channel = server.voice_client
-
-  async with ctx.typing():
-    player = await YTDLSource.from_url(url)
-    voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-  await ctx.send('pqp hein')
-
-
-
-@client.command(pass_context=True)
-async def sucky(ctx):
-  
-  url = 'https://www.myinstants.com/instant/sucky-sucky-5-dolla-78201/?utm_source=copy&utm_medium=share'
-  server = ctx.message.guild
-  voice_channel = server.voice_client
-
-  async with ctx.typing():
-    player = await YTDLSource.from_url(url)
-    voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-  await ctx.send('sucky sucky')
-
-
+plist = []
 @client.command(pass_context=True)
 async def radio_guedes(ctx):
   
   url = 'https://stream-ar.planetradio.co.uk/absoluteclassicrockhigh.aac?direct=true&listenerid=undefined&aw_0_1st.bauer_listenerid=undefined&amsparams=playerid:BMUK_html5;skey:1626468207;&aw_0_1st.playerid=BMUK_html5&aw_0_1st.skey=1626468207&aw_0_1st.bauer_loggedin=false&awparams=loggedin:false;&aw_0_req.userConsentV2=CPJc1m0PJc1m0AGABCENBjCsAP_AAAAAAAKIHANf_X_fb3_D-_59_9t0eY1f9_7_v-0zjgeds-8Nyd_X_L8X_2M7vB36pq4KuR4Eu3LBAQdlHOHcTQmQ6IkVqTPsbk2Mr7NKJ7PEmlMbe2dYGH9_n9XT_ZKY79_____7__-_____77f__-__3_v59UgEAAACQyAyABQAIYATABHACkAGWANSAfYB-AEYAI4AUsAq4BWwDeAJiATYAtEBbAC8wGBAMPAZEAzkBngDPhgAkAbQA8ACxAHVAR6Ak4BdAC8gGhCIDwAVgBDACkAGQAMsAagA2QB-AEAAIwAUsAp4BVwDWAHVAPkAhsBDoCLwEiAJsATsApEBcgDAgGEgMPAZOAzkBnwgAIAbwBdADQgG6BIKoACAAFwAUABUADIAHIAPABAACIAGAAMgAaAA8gCGAIoATAAnwBVAFYALAAbwA5gB6AEIAIaARABEgCOgEsAS4AmgBSgC3AGGAMgAZcA1ADVAGyAO8AewA-IB9gH6AQCAi4CMAEaAI4ASkAoIBSwCngFXALmAX4AxQBrADaAG4AN4AegA-QCGwEOgIvASIAmIBMoCbAE7AKHAUiApoBYoC0AFsALkAXeAvMBgQDBgGEgMNAYeAyIBkgDJwGXAM5AZ8A0gBp0DWANZCAIIAHAAeAH8ARQAkQBmgDaAHOAQMAg4BPwChgGiAOqAh8BHoCVgE2gLCAXQAuoBdoC8gGIAMWAZCAyMBoQDRgGlANTAbcA3QNAlACsAFwAQwApABkADLAGoANkAfgBAACCgEYAKWAU8Aq8BaAFpANYAbwA6oB8gENgIdARUAi8BIgCbAE7AKRAXIAwIBhIDDwGMAMnAZyAzwBnwYAOAbIA6gC6AF9AMjAaEA3QVAdAAoAEMAJgAXABHACkAGWANQAfgBGACOAFLAKvAWgBaQDeAJBATEAmwBTYC2AFyALzAYEAw8BkQDOQGeAM-AbkKAEgDaAHgAQUA6oCPQF0AL6AaEA14dBlAAXABQAFQAMgAcgA-AEAAIgAXQAwADIAGgAPAAfQBDAEUAJgAT4AqgCsAFgALgAXwAxABmADeAHMAPQAhABDQCIAIkAR0AlgCYAE1AKMApQBYgC3gGEAYYAyABlADRAGoANkAb4A7wB7QD7AP0Af4BA4CLAIwARyAlICVAFBAKeAVcAsUBaAFpALmAXUAvIBfgDFAG0ANxAdMB1AD0AIbAQ6AiIBFQCLwEggJEASoAmwBOwChwFNAKsAWKAtCBbAFsgLgAXIAu0Bd4C8wGDAMJAYaAw8BiQDGAGPAMkAZOAyoBlwDOQGfANEgaQBpIDSwGnANYAbGOAiAAIgAcAB4AFwAcgA_ADIAGgAP4AigBIgCzAGWAM0AbQA5wB3AEAAILAQcBCACIgE2gJ8An4BSwCoAF6AMCAZkA1gBvADjgHSAOqAeQA-QCEAEPgI9gSsBK4CYoEyATKAm0BQoCkAFJgKYAVMAqoBWwCuwFlALUAXFAugC6gF9AMCAYgAxYBkIDLwGhQNFA0YBpQDTQGpgNeAbSA2wBtxCB6AAsACgAGQARAAuABiAEMAJgAVQAuABfADEAGYAN4AegBHACkAFiAMIAZQA1ABvgDvgH2AfgA_wCMAEcAJSAUEAoYBTwCrwFoAWkAuYBfgDFAG0AOoAegBIICRAEqAJsAU0AsUBaMC2ALaAXAAuQBdoDDwGJAMiAZOAzkBngDPgGiANJAaWA4AgAlAAQAD8ANAAfwBIgDLAG0AOcAeABBQCfAFLALEAZkA3gB1QDtgIfAR6Ak4BK4CYgE2gKFAUgApMBWwC6AF5AL6AYEA0IBooDSgGpgNsAbcSgcgAIAAWABQADIAHAARQAwADEAHgARAAmABVAC4AF8AMQAZgA2gCEAENAIgAiQBHQCjAKUAW4AwgBlADVAGyAO8AfgBGACOAEnAKeAVeAtAC0gF1AMUAbgA6gB8gEOgIqAReAkQBNgCxQFsALtAXmAw8BkQDJwGcgM8AZ8A0gBrADgCQB0ABwAFwAQgA5ADIAJEAXIAywBqADaAHcAQAAnwBUADMgG8AOqAfYBHoCVgE2gKTAWUAugBfQDFgGhANKAbkUgkAALgAoACoAGQAOQAfACAAEUAMAAyABoADyAIYAigBMACeAFIAKoAWAAvgBiADMAHMAQgAhoBEAETAKMApQBYgC3AGEAMoAaIA1QBsgDvgH2AfoBFgCMAEcAJSAUEAoYBVwCtgFzALyAbQA3AB6AEOgIvASIAk4BNgCdgFDgK2AWKAtgBcAC5AF2gLzAYaAw8BjADIgGSAMnAZcAzkBngDPoGkAaTA1gDWQGxlAGgAFwAQgA5AB-AFYAMgAbQBHACRAFyAMsAagA1wBtADnAHcAPAAgABFQCRAE2AJ3AT4BPwClgFiALqAYoA3gB1QDtgHkAP-Aj0BMQCZQE2gKQAUwAqYBXYC0AF0ALyAX0AwIBiwDQgGiANKAabA1IDUwGvA.YAAAAAAAAAAA&aw_0_1st.bauer_dmp=%5B%22533114%22%2C%22594434%22%2C%22596148%22%2C%22628965%22%2C%22629665%22%2C%22650698%22%5D&aw_0_1st.octave_dmp=%5B%22533114%22%2C%22594434%22%2C%22596148%22%2C%22628965%22%2C%22629665%22%2C%22650698%22%5D'
   channel = ctx.message.author.voice.channel
   global pl
-  try:
-    pl = await channel.connect()
-  except:
-    pass
+
+  pl = await channel.connect()
+
   pl.play(discord.FFmpegPCMAudio(url))
+  
   await ctx.send('s2')
 
 @client.command(pass_context=True)
